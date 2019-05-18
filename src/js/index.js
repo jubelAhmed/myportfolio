aba40ce298a50079e500577bb8a104b62d109d08
jubel <jubel8180@gmail.com>
2019-05-18 21:56:08 +0600

    nav animation add

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,26 @@ for (var i = 0; i < navItem.length; i++) {
 }
 
 
+// when scrolling then active the current nav item --- start
+let mainNavLinks = document.querySelectorAll("nav a ");
+let mainSections = document.querySelectorAll("main section");
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY+10;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+   
+    if ( section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+      link.classList.add("active");
+      link.classList.add("animate-navitem");
+    } else {
+      link.classList.remove("active");
+      link.classList.remove("animate-navitem");
+    }
+  });
+});
+
+// end scroll active
+
+
